92926d1f74b98fcd805498a73056067fdf2dc246
user <karanchadha97>
2021-01-20 19:10:23 +0530

    Today's Work

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\Type\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+
+class TaskController extends AbstractController
+{
+    /**
+    *@Route("/forms",name="task_success")
+     */
+    public function new(Request $request)
+    {
+        $task=new Task();
+       // $task->setDueDate(new DateTime('tomorrow'));
+       /* $task->setTask('Write a blog');
+        $task->setdueDate(new \DateTime('22nd Jan,2021'));*/
+        //$this->createFormBuilder($task)->add('task',TextType::class)->add('dueDate',DateType::class)->add('save',SubmitType::class,['label'=>'Create Task'])->getForm();
+        $form = $this->createForm(TaskType::class,$task);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            /*$task=new Task();
+            $form = $this->createForm(TaskType::class,$task);
+            return $this->render('task/new.html.twig',['form'=>$form->createView()]);*/
+        }
+        //return new Response($form);
+        return $this->render('task/new.html.twig',['form'=>$form->createView()]);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $task;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $dueDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTask(): ?string
+    {
+        return $this->task;
+    }
+
+    public function setTask(string $task): self
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?string
+    {
+       
+        return $this->dueDate;
+    }
+
+    public function setDueDate(string $dueDate): self
+    {
+        //dd(dueDate);
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+}
